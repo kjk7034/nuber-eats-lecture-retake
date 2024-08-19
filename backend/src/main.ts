66e9ab633182427d7c221815fd7eb3115adad32e
kjk7034 <wise7034@gmail.com>
2024-08-19 15:58:08 +0900

    chore(backend): cors 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // Next.js 앱의 도메인
+    credentials: true, // 쿠키 허용
+  };
+  app.enableCors(corsOptions);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
